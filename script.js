a094c205790458dbaee635ec961afd8ba639920f
NotUrKyun <thealtiam2@gmail.com>
2024-03-10 17:24:25 +0900

    Empowerment lol

--- script.js
+++ script.js
@@ -18,11 +18,23 @@ const upObserve = new IntersectionObserver((entries) => {
     })
 })
 
+const rightObserve = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('upshow');
+        }else{
+            entry.target.classList.remove('upshow');
+        }
+    })
+})
 
 const lefthiddenElements = document.querySelectorAll('.leftHover');
+const righthiddenElements = document.querySelectorAll('.rightHover');
 const uphiddenElements = document.querySelectorAll('.upHover');
 
 uphiddenElements.forEach((el) => upObserve.observe(el));
 lefthiddenElements.forEach((el) => observer.observe(el));
+righthiddenElements.forEach((el) => rightObserve.observe(el));
+
 
 console.log("Made by ur dad max");
